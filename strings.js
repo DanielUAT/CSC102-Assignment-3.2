eafb56c1911d9d09c5f70ab81181f845b2268602
DANIEL GROVEMAN <DANGROVE@uat.edu>
2025-11-23 23:12:36 -0600

    first commit

---
+++ strings.js
@@ -0,0 +1,30 @@
+/*var firstName = document.getElementById("firstName").value;
+var lastName = document.getElementById("lastName").value;
+var zipCode = document.getElementById("zipCode").value;
+*/
+function strings() {
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var zipCode = document.getElementById("zipCode").value;
+    // Combine first and last name
+    var combinedName = firstName + " " + lastName;
+    console.log("Combined Name: " + combinedName + " "+ combinedName.length + " characters");
+    var combinedInfo = combinedName + " "  + zipCode;
+    console.log("Combined Info: " + combinedInfo + " " + combinedInfo.length + " characters");
+
+    var secretCode = "";
+    var i;
+    for (i=combinedInfo.length - 1; i >= 0; i--) 
+        {
+            secretCode += combinedInfo.charAt(i);
+            
+        }
+    if (combinedName.length <= 20 && zipCode.length == 5) 
+    {
+        document.getElementById("Secret Code").innerHTML = "Your secret code is: " + secretCode.toLowerCase();
+    }
+    else 
+    {
+        document.getElementById("Secret Code").innerHTML = "Error: Combined name exceeds 20 characters or zip code is not exactly 5 digits.";
+    }
+}
